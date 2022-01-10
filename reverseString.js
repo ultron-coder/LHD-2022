c4e6ce6cf597df13db6b23e7fade65f3af2b09e3
ultron-coder <73244377+ultron-coder@users.noreply.github.com>
2022-01-11 00:39:36 +0530

    Add files via upload

---
+++ reverseString.js
@@ -0,0 +1,51 @@
+/* To reverse the string */
+
+function reverseString(value){
+    // const arrStr = value.split('');
+    // arrStr.reverse();
+    // return arrStr.join('');
+
+            // (or)
+
+    // return value.split('').reverse().join('');
+
+            // (or)
+
+    // let arrStr = '';
+    // for(let i = value.length-1; i >= 0; i--){
+    //     arrStr += value[i];
+    // }
+    /* To remove the word or character from the string using replace() */
+    // dupStr = arrStr.replace('undefined', ''); 
+    // return arrStr;
+
+            // (or)
+
+    // let arrStr = '';
+    // for(let i = 0; i < value.length; i++){
+    //     arrStr = value[i] + arrStr;
+    // }
+    // return arrStr;
+
+            // (or)
+
+    // let arrStr = '';
+    // for( let char of value){
+    //     arrStr = char + arrStr;
+    // }
+    // return arrStr;
+
+            // (or)
+
+    // let arrStr = '';
+    // value.split('').forEach((item) => {
+    //     arrStr = item + arrStr;
+    // })
+    // return arrStr;
+
+            // (or)
+
+    return value.split('').reduce((arrStr, char) => {
+        return char + arrStr;
+    }, '');
+}
